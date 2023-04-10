92663e388de2696ecb43f86b9525d74fb1d2cbb7
Yumin Kim <90626970+miniminii@users.noreply.github.com>
2023-04-10 20:56:15 +0900

    Add files via upload

---
+++ codeup_basic100/1001-1009.py
@@ -0,0 +1,31 @@
+#1001
+print("Hello")
+
+#1002
+print("Hello World")
+
+#1003
+print("Hello")
+print("World")
+
+#1004
+print("'Hello")
+
+#1005
+print('"Hello World"')
+
+#1006
+print('"!@#$%^&*()\'')
+
+#1007
+print("\"C:\\Download\\'hello'.py\"")
+
+#1008
+print("print(\"Hello\\nWorld\")")
+
+#1009
+c = input()
+print(c)
+
+
+
