de112ec26c3442df844a614ea2b0f3dfb0f0d294
Yumin Kim <90626970+miniminii@users.noreply.github.com>
2023-04-11 12:53:57 +0900

    Add files via upload

---
+++ codeup_basic100/1010-1019.py
@@ -0,0 +1,46 @@
+#1010
+c = int(input())
+print(c)
+
+#1011
+f = float(input())
+print(f)
+
+#1012
+a = input()
+b = input()
+print(a)
+print(b)
+
+#1013
+a = input()
+b = input()
+print(a)
+print(a)
+
+#1014
+f = float(input())
+print(f)
+print(f)
+print(f)
+
+#1015
+a,b = input().split()
+print(a)
+print(b)
+
+#1016
+a,b = input().split()
+print(b,a)
+
+#1017
+s = input()
+print(s, s, s)
+
+#1018
+a,b = input().split(':')
+print(a, b, sep = ':')
+
+#1019
+a,b,c = input().split('.')
+print(a,b,c, sep = '-')
